420877cb0a497eae78d27476dad70ac81b07a2b1
Diana Arita <dianaarita500@gmail.com>
2019-01-28 07:05:05 -0600

    version 1

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -1,3 +1,12 @@
-public class GuessGame {
+import java.util.Scanner;
 
+public class GuessGame {
+    public static void main (String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Make a guess: ");
+
+        String n = reader.next();
+        reader.close();
+        System.out.println("You guessed: " + n);
+    }
 }
