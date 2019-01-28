a563b2be99a241edd46373b2087ff84c4005380a
Diana Arita <dianaarita500@gmail.com>
2019-01-28 07:31:37 -0600

    version 3

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -2,13 +2,24 @@ import java.util.Scanner;
 
 public class GuessGame {
     public static void main (String[] args) {
-        Scanner reader = new Scanner(System.in);
+        int val = 20;
 
-        boolean x = true;
+        Scanner reader = new Scanner(System.in);
 
         while(true) {
             System.out.println("Make a guess: ");
             String n = reader.next();
+
+            int foo = Integer.parseInt(n);
+
+            if(foo>val) {
+                System.out.println("Too high");
+            } else if (foo<val) {
+                System.out.println("Too low");
+            } else if (foo==val) {
+                System.out.println("You guessed right!");
+                break;
+            }
 
             if(n.equals("quit")) {
                 break;
