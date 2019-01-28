ae363e3bad6d8f9f35ea058669323a12afdbad36
Diana Arita <dianaarita500@gmail.com>
2019-01-28 07:27:43 -0600

    version 2

--- src/GuessGame.java
+++ src/GuessGame.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class GuessGame {
     public static void main (String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Make a guess: ");
 
-        String n = reader.next();
-        reader.close();
-        System.out.println("You guessed: " + n);
+        boolean x = true;
+
+        while(true) {
+            System.out.println("Make a guess: ");
+            String n = reader.next();
+
+            if(n.equals("quit")) {
+                break;
+            }
+
+            System.out.println("You guessed: " + n);
+        }
     }
 }
